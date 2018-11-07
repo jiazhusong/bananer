e212496db5e72e1b4a1946c8d519947ce4d6add7
jiazhusong <jiazhusong@sina.com>
2018-11-07 15:25:59 +0800

    1

--- src/main.js
+++ src/main.js
@@ -3,9 +3,9 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-
+import Api from './api/index.js';
 Vue.config.productionTip = false
-
+Vue.prototype.$api = Api;
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
@@ -13,3 +13,12 @@ new Vue({
   components: { App },
   template: '<App/>'
 })
+// this.$api.post('user/login.do(地址)', {
+//   "参数名": "参数值"
+// }, response => {
+//   if (response.status >= 200 && response.status < 300) {
+//     console.log(response.data);\\请求成功，response为成功信息参数
+//   } else {
+//     console.log(response.message);\\请求失败，response为失败信息
+//   }
+// });
