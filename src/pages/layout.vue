bbe00d3bd9712d2f21b6c50bfc84d1eef070c879
jiazhusong <jiazhusong@sina.com>
2018-11-07 16:50:46 +0800

    less

---
+++ src/pages/layout.vue
@@ -0,0 +1,29 @@
+/**
+* 作者: jsz
+* 日期: 2018-11-07
+* 描述:
+*/
+<template>
+    <div>
+      1
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "layout",
+        components: {},
+        props: [],
+        data() {
+            return {}
+        },
+        mounted() {
+
+        },
+        methods: {}
+    }
+</script>
+
+<style scoped  lang='less'>
+
+</style>
